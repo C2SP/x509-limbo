aef110061cf508875883c0833f4ccc42bfde0fe8
William Woodruff <william@trailofbits.com>
2023-07-21 15:51:15 -0400

    More testcase fixes (#27)
    * limbo: fix more testcase errors
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * reintroduce basic_constraints kwarg
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    ---------
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- limbo/testcases/_core.py
+++ limbo/testcases/_core.py
@@ -162,11 +162,7 @@ class Builder:
         not_before: datetime = _EPOCH,
         not_after: datetime = _ONE_THOUSAND_YEARS_OF_TORMENT,
         key: PrivateKeyTypes | None = None,
-        basic_constraints: _Extension[x509.BasicConstraints]
-        | None = ext(
-            x509.BasicConstraints(ca=True, path_length=None),
-            critical=True,
-        ),
+        basic_constraints: _Extension[x509.BasicConstraints] | None = None,
         key_usage: _Extension[x509.KeyUsage]
         | None = ext(
             x509.KeyUsage(
@@ -197,16 +193,21 @@ class Builder:
         * That certificates are correctly uniqued by both their key **and** their
           subject (as each intermediate generated here shares the same key)
         """
+        if pathlen is not None and basic_constraints is not None:
+            raise ValueError("supply only one of pathlen and basic_constraints")
+
         if not issuer:
             issuer = parent.cert.subject
 
         if not subject:
-            subject = x509.Name.from_rfc4514_string(f"CN=x509-limbo-intermediate-pathlen-{pathlen}")
+            # NOTE: Stuff the parent cert's SN into the subject here to break accidental
+            # self-issuing chains.
+            subject = x509.Name.from_rfc4514_string(
+                f"CN=x509-limbo-intermediate-pathlen-{pathlen},OU={parent.cert.serial_number}"
+            )
 
         if not basic_constraints:
-            basic_constraints = ext(
-                x509.BasicConstraints(True, path_length=pathlen), critical=False
-            )
+            basic_constraints = ext(x509.BasicConstraints(True, path_length=pathlen), critical=True)
 
         return self._ca(
             issuer,
