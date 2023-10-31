e5e9cc633794d12a163957731cf3f0f8e1fede20
William Woodruff <william@trailofbits.com>
2023-10-31 13:07:39 -0400

    RFC5280: add serial number cases (#60)
    * RFC5280: add serial number cases
    
    These are marked with a "pedantic" feature flag,
    since many implementations choose not to enforce them.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * fix import
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    ---------
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- limbo/testcases/rfc5280.py
+++ limbo/testcases/rfc5280.py
@@ -2,6 +2,7 @@
 RFC5280 profile tests.
 """
 
+import random
 from datetime import datetime
 from ipaddress import IPv4Address, IPv4Network
 
@@ -142,9 +143,6 @@ def unknown_critical_extension_intermediate(builder: Builder) -> None:
         .peer_certificate(leaf)
         .fails()
     )
-
-
-# TODO: Empty serial number, overlength serial number.
 
 
 @testcase
@@ -1406,6 +1404,44 @@ def san_noncritical_with_empty_subject(builder: Builder) -> None:
     )
 
     builder = builder.server_validation()
+    builder.trusted_certs(root).peer_certificate(leaf).expected_peer_name(
+        PeerName(kind="DNS", value="example.com")
+    ).fails()
+
+
+@testcase
+def serial_number_too_long(builder: Builder) -> None:
+    """
+    Produces an **invalid** chain due to an invalid EE cert.
+
+    The EE cert contains a serial number longer than 20 octets, which is
+    disallowed under RFC 5280.
+    """
+
+    root = builder.root_ca()
+    # NOTE: Intentionally generate 22 octets, since many implementations are
+    # permissive of 21-octet encodings due to signedness errors.
+    leaf = builder.leaf_cert(root, serial=int.from_bytes(random.randbytes(22), signed=False))
+
+    builder = builder.server_validation().features([Feature.pedantic_serial_number])
+    builder.trusted_certs(root).peer_certificate(leaf).expected_peer_name(
+        PeerName(kind="DNS", value="example.com")
+    ).fails()
+
+
+@testcase
+def serial_number_zero(builder: Builder) -> None:
+    """
+    Produces an **invalid** chain due to an invalid EE cert.
+
+    The EE cert contains a serial number of zero, which is disallowed
+    under RFC 5280.
+    """
+
+    root = builder.root_ca()
+    leaf = builder.leaf_cert(root, serial=0)
+
+    builder = builder.server_validation().features([Feature.pedantic_serial_number])
     builder.trusted_certs(root).peer_certificate(leaf).expected_peer_name(
         PeerName(kind="DNS", value="example.com")
     ).fails()
