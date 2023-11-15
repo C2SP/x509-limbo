a53226a24d80e3b3dbb63174f4575b274ab17e44
William Woodruff <william@trailofbits.com>
2023-11-15 14:49:58 -0500

    limbo: use more submodules, more webpki AKI tests (#86)
    * limbo: use more submodules, more webpki AKI tests
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * lintage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    ---------
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- limbo/testcases/webpki/__init__.py
+++ limbo/testcases/webpki/__init__.py
@@ -5,12 +5,13 @@ Web PKI (CA/B Forum) profile tests.
 from datetime import datetime
 
 from cryptography import x509
-from cryptography.hazmat._oid import NameOID
 from cryptography.hazmat.primitives.asymmetric import dsa, ec
 
 from limbo.assets import ASSETS_PATH, Certificate, ext
 from limbo.models import Feature, KeyUsage, KnownEKUs, PeerName
 from limbo.testcases._core import Builder, testcase
+
+from .aki import *  # noqa: F403
 
 
 @testcase
@@ -461,110 +462,6 @@ def root_with_extkeyusage(builder: Builder) -> None:
         .peer_certificate(leaf)
         .fails()
     )
-
-
-@testcase
-def root_with_aki_authoritycertissuer(builder: Builder) -> None:
-    """
-    Produces the following **invalid** chain:
-
-    ```
-    root -> EE
-    ```
-
-    The root cert includes the authorityKeyIdentifier extension with the
-    authorityCertIssuer field, which is forbidden under the [CA/B BR profile]:
-
-    > 7.1.2.1.3 Root CA Authority Key Identifier
-    > Field                 Description
-    > ...
-    > authorityCertIssuer   MUST NOT be present
-
-    [CA/B BR profile]: https://cabforum.org/wp-content/uploads/CA-Browser-Forum-BR-v2.0.0.pdf
-    """
-
-    key = ec.generate_private_key(ec.SECP256R1())
-    dirname = x509.DirectoryName(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, "myCN")]))
-    aki = x509.AuthorityKeyIdentifier.from_issuer_public_key(key.public_key())
-    # Manually add the authorityCertIssuer field, since cryptography's API doesn't allow to
-    # add it without also specifying the authorityCertSerialNumber field
-    aki._authority_cert_issuer = [dirname]
-
-    root = builder.root_ca(key=key, aki=ext(aki, critical=False))
-    leaf = builder.leaf_cert(root)
-
-    builder = builder.server_validation()
-    builder = builder.trusted_certs(root).peer_certificate(leaf).fails()
-
-
-@testcase
-def root_with_aki_authoritycertserialnumber(builder: Builder) -> None:
-    """
-    Produces the following **invalid** chain:
-
-    ```
-    root -> EE
-    ```
-
-    The root cert includes the authorityKeyIdentifier extension with the
-    authorityCertSerialNumber field, which is forbidden under the
-    [CA/B BR profile]:
-
-    > 7.1.2.1.3 Root CA Authority Key Identifier
-    > Field                         Description
-    > ...
-    > authorityCertSerialNumber     MUST NOT be present
-
-    [CA/B BR profile]: https://cabforum.org/wp-content/uploads/CA-Browser-Forum-BR-v2.0.0.pdf
-    """
-
-    key = ec.generate_private_key(ec.SECP256R1())
-    aki = x509.AuthorityKeyIdentifier.from_issuer_public_key(key.public_key())
-    # Manually add the authorityCertSerialNumber field, since cryptography's
-    # API doesn't allow to add it without also specifying the
-    # authorityCertIssuer field
-    aki._authority_cert_serial_number = 1234
-
-    root = builder.root_ca(key=key, aki=ext(aki, critical=False))
-    leaf = builder.leaf_cert(root)
-
-    builder = builder.server_validation()
-    builder = builder.trusted_certs(root).peer_certificate(leaf).fails()
-
-
-@testcase
-def root_with_aki_all_fields(builder: Builder) -> None:
-    """
-    Produces the following **invalid** chain:
-
-    ```
-    root -> EE
-    ```
-
-    The root cert includes the authorityKeyIdentifier extension with the
-    authorityCertIssuer and authorityCertSerialNumber fields, which is
-    forbidden under the [CA/B BR profile]:
-
-    > 7.1.2.1.3 Root CA Authority Key Identifier
-    > Field                         Description
-    > ...
-    > authorityCertIssuer           MUST NOT be present
-    > authorityCertSerialNumber     MUST NOT be present
-
-    [CA/B BR profile]: https://cabforum.org/wp-content/uploads/CA-Browser-Forum-BR-v2.0.0.pdf
-    """
-
-    key = ec.generate_private_key(ec.SECP256R1())
-    key_identifier = x509.AuthorityKeyIdentifier.from_issuer_public_key(
-        key.public_key()
-    ).key_identifier
-    dirname = x509.DirectoryName(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, "myCN")]))
-    aki = x509.AuthorityKeyIdentifier(key_identifier, [dirname], 1234)
-    root = builder.root_ca(key=key, aki=ext(aki, critical=False))
-    leaf = builder.leaf_cert(root)
-
-    builder = builder.server_validation()
-    builder = builder.trusted_certs(root).peer_certificate(leaf).fails()
 
 
 @testcase
