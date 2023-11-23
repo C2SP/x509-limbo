c3f0f4b5d90b55fda786e97b305ca41351239e47
William Woodruff <william@trailofbits.com>
2023-11-23 18:08:46 -0500

    limbo: add rfc5280::expired-intermediate
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- limbo/testcases/rfc5280/__init__.py
+++ limbo/testcases/rfc5280/__init__.py
@@ -910,3 +910,48 @@ def malformed_subject_alternative_name(builder: Builder) -> None:
 
     builder = builder.server_validation()
     builder = builder.trusted_certs(root).peer_certificate(leaf).fails()
+
+
+@testcase
+def expired_intermediate(builder: Builder) -> None:
+    """
+    Produces the following **invalid** chain:
+
+    ```
+    root -> intermediate -> EE
+    ```
+
+    All three certificates are well-formed, but the intermediate
+    (and only the intermediate) is expired at the validation time.
+    """
+
+    # Root is valid from 2016 to 2026.
+    root = builder.root_ca(
+        not_before=datetime.fromisoformat("2016-01-01T00:00:00Z"),
+        not_after=datetime.fromisoformat("2026-01-01T00:00:00Z"),
+    )
+
+    # Intermediate is valid from 2016 to 2020.
+    intermediate = builder.intermediate_ca(
+        root,
+        not_before=datetime.fromisoformat("2016-01-01T00:00:00Z"),
+        not_after=datetime.fromisoformat("2020-01-01T00:00:00Z"),
+    )
+
+    # Leaf is valid from 2018 to 2023.
+    leaf = builder.leaf_cert(
+        intermediate,
+        not_before=datetime.fromisoformat("2016-01-01T00:00:00Z"),
+        not_after=datetime.fromisoformat("2023-01-01T00:00:00Z"),
+    )
+
+    builder = (
+        builder.server_validation()
+        .trusted_certs(root)
+        .untrusted_intermediates(intermediate)
+        .peer_certificate(leaf)
+        # We validate in 2022, which is valid for the root and leaf
+        # but not the intermediate.
+        .validation_time(datetime.fromisoformat("2022-01-01T00:00:00Z"))
+        .fails()
+    )
