9d878bf7f1084c9c33b09bc8ac2e2701ecfa40eb
William Woodruff <william@trailofbits.com>
2023-11-23 19:44:50 -0500

    limbo: add rfc5280::expired-intermediate (#98)
    * limbo: add rfc5280::expired-intermediate
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * limbo: expired-root
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * limbo: expired-leaf
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * limbo: rfc5280: more namespacing
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * lintage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * render-summary: prevent emojification
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    ---------
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

---
+++ limbo/testcases/rfc5280/ski.py
@@ -0,0 +1,63 @@
+"""
+RFC 5280 Subject Key Identifier (SKI) testcases.
+"""
+
+from cryptography import x509
+from cryptography.hazmat.primitives.asymmetric import ec
+
+from limbo.testcases._core import Builder, ext, testcase
+
+
+@testcase
+def critical_ski(builder: Builder) -> None:
+    """
+    Produces the following **invalid** chain:
+
+    ```
+    root -> EE
+    ```
+
+    The root cert has an SKI extension marked as critical, which is disallowed
+    under the [RFC 5280 profile].
+
+    > Conforming CAs MUST mark this extension as non-critical.
+
+    [RFC 5280 profile]: https://datatracker.ietf.org/doc/html/rfc5280#section-4.2.1.2
+    """
+    key = ec.generate_private_key(ec.SECP256R1())
+    root = builder.root_ca(
+        ski=ext(x509.SubjectKeyIdentifier.from_public_key(key.public_key()), critical=True),
+    )
+    leaf = builder.leaf_cert(root)
+
+    builder = builder.server_validation()
+    builder = builder.trusted_certs(root).peer_certificate(leaf).fails()
+
+
+@testcase
+def missing_ski(builder: Builder) -> None:
+    """
+    Produces the following **invalid** chain:
+
+    ```
+    root -> EE
+    ```
+
+    The root cert is missing the SKI extension, which is disallowed under the
+    [RFC 5280 profile].
+
+    > To facilitate certification path construction, this extension MUST
+    > appear in all conforming CA certificates, that is, all certificates
+    > including the basic constraints extension (Section 4.2.1.9) where the
+    > value of cA is TRUE.
+
+    Note: for roots, the SKI should be the same value as the AKI, therefore,
+    this extension isn't strictly necessary, although required by the RFC.
+
+    [RFC 5280 profile]: https://datatracker.ietf.org/doc/html/rfc5280#section-4.2.1.2
+    """
+    root = builder.root_ca(ski=None)
+    leaf = builder.leaf_cert(root)
+
+    builder = builder.server_validation()
+    builder = builder.trusted_certs(root).peer_certificate(leaf).fails()
