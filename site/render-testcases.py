5b9aac3e4db49ee4d756854d2daae0e1fd633f0b
William Woodruff <william@trailofbits.com>
2023-11-29 17:57:30 +0100

    site: more rendering tweaks (#109)
    Templatize a bit, add a table.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- site/render-testcases.py
+++ site/render-testcases.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-# render-testcases: convert `limbo.json` into a pretty page
+# render-testcases: convert `limbo.json` into pretty pages
+# TODO(ww): Use some kind of Markdown builder API here, rather than
+# smashing strings together.
 
 from collections import defaultdict
 from pathlib import Path
@@ -12,18 +14,36 @@ from limbo.models import Limbo, Testcase
 LIMBO_JSON = Path(__file__).parent.parent / "limbo.json"
 assert LIMBO_JSON.is_file()
 
+TESTCASE_TEMPLATE = """
+## {tc_id}
+
+{description}
+
+| Expected result | Validation kind | Validation time | Features   |
+| --------------- | --------------- | --------------- | ---------- |
+| {exp_result}    | {val_kind}      | {val_time}      | {features} |
+"""
+
 limbo = Limbo.parse_file(LIMBO_JSON)
 
-groups: dict[str, list[Testcase]] = defaultdict(list)
+namespaces: dict[str, list[Testcase]] = defaultdict(list)
 for tc in limbo.testcases:
-    group, _ = tc.id.split("::", 1)
-    groups[group].append(tc)
+    namespace, _ = tc.id.split("::", 1)
+    namespaces[namespace].append(tc)
 
-for group, tcs in groups.items():
-    with mkdocs_gen_files.open(f"testcases/{group}.md", "w") as f:
-        print(f"# {group}", file=f)
+for namespace, tcs in namespaces.items():
+    with mkdocs_gen_files.open(f"testcases/{namespace}.md", "w") as f:
+        print(f"# {namespace}", file=f)
 
         for tc in tcs:
-            print(f"## `{tc.id}`", file=f)
-
-            print(tc.description, file=f)
+            print(
+                TESTCASE_TEMPLATE.format(
+                    tc_id=tc.id,
+                    exp_result=tc.expected_result.value,
+                    val_kind=tc.validation_kind.value,
+                    val_time=tc.validation_time.isoformat() if tc.validation_time else "N/A",
+                    features=", ".join([f.value for f in tc.features]) if tc.features else "N/A",
+                    description=tc.description,
+                ),
+                file=f,
+            )
