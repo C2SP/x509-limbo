515ceb041eddcce44a177038232982897499f523
William Woodruff <william@trailofbits.com>
2023-11-30 23:37:16 +0100

    webpki: devolve eku tests
    Signed-off-by: William Woodruff <william@trailofbits.com>

---
+++ limbo/testcases/webpki/eku.py
@@ -0,0 +1,69 @@
+from cryptography import x509
+
+from limbo.assets import ext
+from limbo.models import Feature, KnownEKUs, PeerName
+from limbo.testcases._core import Builder, testcase
+
+
+@testcase
+def ee_anyeku(builder: Builder) -> None:
+    """
+    Produces the following **invalid** chain:
+
+    ```
+    root -> EE
+    ```
+
+    This chain is correctly constructed, but the EE cert contains an
+    Extended Key Usage extension that contains `anyExtendedKeyUsage`,
+    which is explicitly forbidden under CABF 7.1.2.7.10.
+    """
+
+    root = builder.root_ca()
+    leaf = builder.leaf_cert(
+        root,
+        eku=ext(
+            x509.ExtendedKeyUsage(
+                [x509.OID_SERVER_AUTH, x509.ExtendedKeyUsageOID.ANY_EXTENDED_KEY_USAGE]
+            ),
+            critical=False,
+        ),
+    )
+
+    # NOTE: Marked as pedantic since most implementations don't seem to care.
+    builder = builder.server_validation().features([Feature.pedantic_webpki_eku])
+    builder.trusted_certs(root).peer_certificate(leaf).expected_peer_name(
+        PeerName(kind="DNS", value="example.com")
+    ).extended_key_usage([KnownEKUs.server_auth]).fails()
+
+
+@testcase
+def root_has_eku(builder: Builder) -> None:
+    """
+    Produces the following **invalid** chain:
+
+    ```
+    root -> EE
+    ```
+
+    The root cert includes the extKeyUsage extension, which is forbidden
+    under CABF:
+
+    > 7.1.2.1.2 Root CA Extensions
+    > Extension     Presence        Critical
+    > ...
+    > extKeyUsage   MUST NOT        N
+    """
+
+    root = builder.root_ca(
+        extra_extension=ext(x509.ExtendedKeyUsage([x509.OID_SERVER_AUTH]), critical=False)
+    )
+    leaf = builder.leaf_cert(root)
+
+    builder = builder.server_validation().features([Feature.pedantic_webpki_eku])
+    builder = (
+        builder.trusted_certs(root)
+        .extended_key_usage([KnownEKUs.server_auth])
+        .peer_certificate(leaf)
+        .fails()
+    )
