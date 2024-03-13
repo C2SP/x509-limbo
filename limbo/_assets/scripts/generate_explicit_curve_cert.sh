a04fb05cf132e1405f71c12616cf0aead829909a
William Woodruff <william@yossarian.net>
2024-03-13 10:54:59 -0400

    Add an explicit curve test (#173)
    * _assets: begin hacking up an explicit curve test
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * limbo: add an explicit EC curve test
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * limbo: fix extensions
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    ---------
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

---
+++ limbo/_assets/scripts/generate_explicit_curve_cert.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+set -e
+
+ca_csr_conf='
+[ req ]
+default_md = sha256
+prompt = no
+req_extensions = req_ext
+distinguished_name = req_distinguished_name
+[ req_distinguished_name ]
+commonName = x509-limbo-explicit-ec-root
+[ req_ext ]
+keyUsage=critical,keyCertSign,cRLSign
+basicConstraints=critical,CA:true
+'
+
+leaf_csr_conf='
+[ req ]
+default_md = sha256
+prompt = no
+req_extensions = req_ext
+distinguished_name = req_distinguished_name
+[ req_distinguished_name ]
+commonName = example.com
+[ req_ext ]
+keyUsage=critical,digitalSignature,keyEncipherment
+extendedKeyUsage=serverAuth,clientAuth
+basicConstraints=critical,CA:false
+subjectAltName = @alt_names
+[ alt_names ]
+DNS.0 = example.com
+'
+
+# CA CSR
+openssl req \
+    -new \
+    -noenc \
+    -key ../explicit_curve_ca.key \
+    -config <(echo "${ca_csr_conf}") \
+    -nameopt utf8 \
+    -utf8 \
+    -out ../explicit_curve_ca.csr
+
+# Self-signed CA (with explicit EC encoding)
+openssl x509 \
+    -signkey ../explicit_curve_ca.key \
+    -days 365 \
+    -req -in ../explicit_curve_ca.csr \
+    -copy_extensions=copyall \
+    -out ../explicit_curve_ca.pem
+
+# Leaf CSR
+openssl req \
+    -new \
+    -noenc \
+    -key ../explicit_curve_leaf.key \
+    -config <(echo "${leaf_csr_conf}") \
+    -nameopt utf8 \
+    -utf8 \
+    -out ../explicit_curve_leaf.csr
+
+# Leaf certificate (with explicit EC encoding)
+openssl x509 \
+    -days 365 \
+    -req -in ../explicit_curve_leaf.csr \
+    -copy_extensions=copyall \
+    -CA ../explicit_curve_ca.pem \
+    -CAkey ../explicit_curve_ca.key \
+    -out ../explicit_curve_leaf.pem
