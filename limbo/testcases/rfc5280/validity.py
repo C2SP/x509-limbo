95c68a84251c8b6c92656d688a56c8f640160918
William Woodruff <william@trailofbits.com>
2024-03-18 17:23:32 -0400

    limbo: add notafter_fractional
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- limbo/testcases/rfc5280/validity.py
+++ limbo/testcases/rfc5280/validity.py
@@ -2,7 +2,7 @@
 RFC 5280 validity testcases.
 """
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from limbo.testcases._core import Builder, testcase
 
@@ -178,5 +178,60 @@ def notafter_exact(builder: Builder) -> None:
         .untrusted_intermediates(ica)
         .peer_certificate(leaf)
         .validation_time(not_after)
+        .succeeds()
+    )
+
+
+@testcase
+def notafter_fractional(builder: Builder) -> None:
+    """
+    Produces the following **ambiguous** chain:
+
+    ```
+    root -> ICA -> EE
+    ```
+
+    EE expires at `2024-04-01T00:00:00Z`, and the chain is validated at
+    `2024-04-01T00:00:00.005Z`, i.e. 5 milliseconds after the `notAfter`
+    date.
+
+    RFC 5280 only allows second granularities in the validity interval, with
+    two conflicting interpretations of how to handle the validity check:
+
+    1. Comparisons are performed at the granularity of the encoded
+       representation, i.e. `floor(time)`. Under this interpretation,
+       the chain is valid, since the entire millisecond interval `[0, .999...]`
+       is truncated to `0`.
+    2. Comparisons are instantaneous. Under this interpretation the chain
+       is **invalid**, since 5 milliseconds after the `notAfter` is factually
+       after the `notAfter`.
+
+    There is no clear "winning" interpretation here, although
+    CAs in the Web PKI have filed and handled compliance reports based on
+    interpretation (1).
+
+    See also:
+
+    * <https://bugzilla.mozilla.org/show_bug.cgi?id=1715455>
+    * <https://groups.google.com/a/mozilla.org/g/dev-security-policy/c/-BogZx_IJyk/m/gHm3l613AgAJ>
+    """
+
+    not_before = datetime.fromisoformat("2024-03-01T00:00:00Z")
+    not_after = datetime.fromisoformat("2024-04-01T00:00:00Z")
+
+    root = builder.root_ca(not_before=not_before, not_after=not_after)
+    ica = builder.intermediate_ca(root, not_before=not_before, not_after=not_after)
+    leaf = builder.leaf_cert(
+        ica,
+        not_before=not_before,
+        not_after=not_after,
+    )
+
+    builder = (
+        builder.server_validation()
+        .trusted_certs(root)
+        .untrusted_intermediates(ica)
+        .peer_certificate(leaf)
+        .validation_time(not_after + timedelta(milliseconds=5))
         .succeeds()
     )
