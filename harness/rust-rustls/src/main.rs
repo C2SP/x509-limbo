8025b01d65d53d28f770b77c94ee106a96c123c1
Daniel McCarney <daniel@binaryparadox.net>
2025-05-24 10:30:54 -0400

    harness/rust-rustls: add CRL revocation checking support

--- harness/rust-rustls/src/main.rs
+++ harness/rust-rustls/src/main.rs
@@ -27,16 +27,19 @@ fn cert_der_from_pem<B: AsRef<[u8]>>(bytes: B) -> rustls_pki_types::CertificateD
     rustls_pki_types::CertificateDer::from(pem.contents()).into_owned()
 }
 
+fn crl_der_from_pem<B: AsRef<[u8]>>(
+    bytes: B,
+) -> rustls_pki_types::CertificateRevocationListDer<'static> {
+    let pem = pem::parse(bytes).expect("crl: PEM parse failed");
+    rustls_pki_types::CertificateRevocationListDer::from(pem.into_contents())
+}
+
 fn evaluate_testcase(tc: &Testcase) -> TestcaseResult {
     if tc.features.contains(&Feature::MaxChainDepth) {
         return TestcaseResult::skip(
             tc,
             "max-chain-depth testcases are not supported by this API",
         );
-    }
-
-    if tc.features.contains(&Feature::HasCrl) {
-        return TestcaseResult::skip(tc, "CRLs are not supported by this API");
     }
 
     if !matches!(tc.validation_kind, ValidationKind::Server) {
@@ -93,13 +96,34 @@ fn evaluate_testcase(tc: &Testcase) -> TestcaseResult {
         ring::RSA_PSS_2048_8192_SHA512_LEGACY_KEY,
     ];
 
+    let crls = tc
+        .crls
+        .iter()
+        .map(|pem| {
+            webpki::OwnedCertRevocationList::from_der(crl_der_from_pem(pem).as_ref())
+                .unwrap_or_else(|e| panic!("crl: tc {} DER parse failed: {e}", tc.id.to_string()))
+                .into()
+        })
+        .collect::<Vec<_>>();
+    let crls = crls.iter().collect::<Vec<_>>();
+
+    let revocation_options = if !crls.is_empty() {
+        let opts = webpki::RevocationOptionsBuilder::new(crls.as_slice()).unwrap();
+        opts.with_depth(webpki::RevocationCheckDepth::Chain);
+        opts.with_status_policy(webpki::UnknownStatusPolicy::Deny);
+        opts.with_expiration_policy(webpki::ExpirationPolicy::Enforce);
+        Some(opts.build())
+    } else {
+        None
+    };
+
     if let Err(e) = leaf.verify_for_usage(
         sig_algs,
         &trust_anchors,
         &intermediates[..],
         validation_time,
         webpki::KeyUsage::server_auth(),
-        None,
+        revocation_options,
         None,
     ) {
         return TestcaseResult::fail(tc, &e.to_string());
